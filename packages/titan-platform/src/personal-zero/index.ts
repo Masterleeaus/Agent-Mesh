31da83701b355d5b0de3b499800cdc7ef9da57c1
IOSONOSOI <masterlee.aus@gmail.com>
2026-09-23 06:46:11 +1000

    feat: export accepted learning consumption

--- packages/titan-platform/src/personal-zero/index.ts
+++ packages/titan-platform/src/personal-zero/index.ts
@@ -2,3 +2,4 @@ export * from "./contracts.js";
 export * from "./state-service.js";
 export * from "./learning-governor-bridge.js";
 export * from "./learning-review.js";
+export * from "./learning-consumption.js";
