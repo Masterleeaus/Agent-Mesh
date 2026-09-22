5df0465d21520c1ea9620d253ce43c035c6c87e6
IOSONOSOI <masterlee.aus@gmail.com>
2026-09-22 15:16:51 +1000

    marketing: route new Field Services differentiation pages

--- apps/marketing-nexjob/src/App.jsx
+++ apps/marketing-nexjob/src/App.jsx
@@ -8,6 +8,11 @@ import Industries from './pages/Industries'
 import About from './pages/About'
 import FAQ from './pages/FAQ'
 import Changelog from './pages/Changelog'
+import ManagedSystem from './pages/ManagedSystem'
+import Architecture from './pages/Architecture'
+import CostSovereignty from './pages/CostSovereignty'
+import EnvironmentalSystems from './pages/EnvironmentalSystems'
+import Compare from './pages/Compare'
 import ScrollToTop from './components/ScrollToTop'
 import CookieConsent from './components/CookieConsent'
 
@@ -21,6 +26,11 @@ export default function App() {
         <Route path="/features" element={<Features />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/industries" element={<Industries />} />
+        <Route path="/fully-managed" element={<ManagedSystem />} />
+        <Route path="/privacy-architecture" element={<Architecture />} />
+        <Route path="/cost-sovereignty" element={<CostSovereignty />} />
+        <Route path="/environmental-systems" element={<EnvironmentalSystems />} />
+        <Route path="/compare" element={<Compare />} />
         <Route path="/about" element={<About />} />
         <Route path="/faq" element={<FAQ />} />
         <Route path="/changelog" element={<Changelog />} />
