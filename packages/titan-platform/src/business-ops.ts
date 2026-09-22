180b93537669bd156b950274a9860e0ef17a857b
IOSONOSOI <masterlee.aus@gmail.com>
2026-09-22 15:20:09 +1000

    feat(#724): export warranty contracts through business ops

--- packages/titan-platform/src/business-ops.ts
+++ packages/titan-platform/src/business-ops.ts
@@ -1,3 +1,4 @@
+export * from "./warranty.js";
 import { classifyRisk, type TitanRiskAssessment } from "./intelligence.js";
 import { routeOperationalRole, type TitanRoleRouteResult } from "./workforce.js";
 
