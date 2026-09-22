60b2e576d2453e45fa9280de30fd5a65781aebf3
IOSONOSOI <masterlee.aus@gmail.com>
2026-09-22 16:25:07 +1000

    Fix worker package convergence

--- services/worker/src/review-request.ts
+++ services/worker/src/review-request.ts
@@ -1,6 +1,6 @@
 import type { Client } from "pg";
 import { logger } from "./logger.js";
-import { reviewRequestEmailHtml } from "@titan-zero/email-templates";
+import { reviewRequestEmailHtml } from "@ai-fsm/email-templates";
 import type { AutomationRow, RunResult } from "./automations/types.js";
 import { enqueueNotification } from "./notification/enqueue.js";
 import { PRIORITY } from "./notification/priority.js";
